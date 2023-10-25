50e4285bc06d1e3f7f8e84548f9f6e3de03d120d
Chase-Scripts <112039422+vk-chase@users.noreply.github.com>
2023-10-25 18:42:38 -0500

    Update server.lua
    same update following client.lua update

--- server/server.lua
+++ server/server.lua
@@ -1,67 +1,66 @@
 QBCore = exports['qb-core']:GetCoreObject()
 
-RegisterServerEvent("Farmcyc:recoleccion", function()
+RegisterServerEvent("mudbug:server:getreward", function()
 	local src = source
 	local Player = QBCore.Functions.GetPlayer(src)
-	local recoleccionsuerte = math.random(1,100)
-        if recoleccionsuerte < 50 then
-				Player.Functions.AddItem("crabs", math.random(1,2))
-        	elseif recoleccionsuerte > 51 then
+	local chancex = math.random(1,100)
+    if chancex < 50 then
+			Player.Functions.AddItem("crabs", math.random(1,2))
+			TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items["crabs"], "add")
+    elseif chancex > 51 then
 		Player.Functions.AddItem("shrimp", math.random(1,2))
-    end
+		TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items["shrimp"], "add")
+	end
 end)
 
-
-
-QBCore.Functions.CreateCallback('Farmcyc:empaquetado:cangrejos', function (source, cb)
+QBCore.Functions.CreateCallback('mudbug:server:crabsprocessed', function (source, cb)
 	local src = source
 	local Player  = QBCore.Functions.GetPlayer(src)
-	if Player.Functions.GetItemByName('crabs') then
-		Player.Functions.RemoveItem('crabs', 5)
+	if Player.Functions.RemoveItem('crabs', Config.CrabPerBox) then
+		TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items["crabs"], "remove")
 			Wait(1000)
 		Player.Functions.AddItem('crayfish', 1)
+		TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items["crayfish"], "add")
 	else
-		TriggerClientEvent('QBCore:Notify', src, "You have no crabs", 'error')
+		TriggerClientEvent('QBCore:Notify', src, "You need "..Config.CrabPerBox.." crabs to process", 'error')
     end
 	cb(false)
 end)
 
-QBCore.Functions.CreateCallback('Farmcyc:empaquetado:camarones', function (source, cb)
+QBCore.Functions.CreateCallback('mudbug:server:shrimpprocessed', function (source, cb)
 	local src = source
 	local Player  = QBCore.Functions.GetPlayer(src)
-	if Player.Functions.GetItemByName('shrimp') then
-		Player.Functions.RemoveItem('shrimp', 4)
+	if Player.Functions.RemoveItem('shrimp', Config.ShrimpPerBox) then
+		TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items["shrimp"], "remove")
 			Wait(1000)
 		Player.Functions.AddItem('shrimpbox', 1)
+		TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items["shrimpbox"], "add")
 	else
-		TriggerClientEvent('QBCore:Notify', src, "You have no shrimp", 'error')
+		TriggerClientEvent('QBCore:Notify', src, "You need "..Config.ShrimpPerBox.." shrimp to process", 'error')
 	end
 	cb(true)
 end)
 
-
-RegisterNetEvent('Ventadecamarones', function()
+RegisterNetEvent('mudbug:server:paymentsale1', function()
 	local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    local PrecioCamarones = Config.PCamaron
-	if Player.Functions.GetItemByName('shrimpbox') then
-		Player.Functions.RemoveItem('shrimpbox', 1)
-		Player.Functions.AddMoney('cash', PrecioCamarones)
+    local amount = Config.ProcessedShrimpPayout
+	if Player.Functions.RemoveItem('shrimpbox', Config.ShrimBoxRequired) then
+		TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items["shrimpbox"], "remove")
+		Player.Functions.AddMoney('cash', amount)
 	else
-		TriggerClientEvent('QBCore:Notify', src, "You have no Shrimps", 'error')
+		TriggerClientEvent('QBCore:Notify', src, "You need "..Config.ShrimBoxRequired.." Shrimp Boxes", 'error')
 	end
 end)
 
-
-
-RegisterNetEvent('Ventadecangrejos', function()
+RegisterNetEvent('mudbug:server:paymentsale2', function()
 	local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    local PrecioCangrejos = Config.PCangrejo
-	if Player.Functions.GetItemByName('crayfish') then
-		Player.Functions.RemoveItem('crayfish', 1)
-		Player.Functions.AddMoney('cash', PrecioCangrejos)
+    local amount = Config.CrawFishPayout
+	if Player.Functions.RemoveItem('crayfish', Config.CrawfishRequired) then
+		TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items["shrimpbox"], "remove")
+		Player.Functions.AddMoney(Config.PayType, amount)
 	else
-		TriggerClientEvent('QBCore:Notify', src, "You have no crabs", 'error')
+		TriggerClientEvent('QBCore:Notify', src, "You will need "..Config.CrawfishRequired.." Crawfish", 'error')
 	end
 end)
