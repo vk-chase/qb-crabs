285d3a00b2828429fcdb1243daae02e7f43dba75
Chase-Scripts <112039422+vk-chase@users.noreply.github.com>
2023-10-25 23:09:31 -0500

    Update Items.lua
    x

--- Items.lua
+++ Items.lua
@@ -3,5 +3,4 @@
 
 	['crabs'] 			 			 = {['name'] = 'crabs', 						['label'] = 'Crabs', 					['weight'] = 1000, 		['type'] = 'item', 		['image'] = 'crabs.png', 				['unique'] = false, 		['useable'] = true, 	['shouldClose'] = true,	   ['combinable'] = nil,   ['description'] = 'Crabs'},
 	['shrimp'] 			 			 = {['name'] = 'shrimp', 						['label'] = 'Shrimp', 					['weight'] = 1000, 		['type'] = 'item', 		['image'] = 'shrimp.png', 				['unique'] = false, 		['useable'] = true, 	['shouldClose'] = true,	   ['combinable'] = nil,   ['description'] = 'Shrimp'},
-	['shrimpbox'] 		 			 = {['name'] = 'shrimpbox', 					['label'] = 'Shrimp Box', 				['weight'] = 6000, 		['type'] = 'item', 		['image'] = 'shrimpbox.png', 			['unique'] = true, 		['useable'] = true, 	['shouldClose'] = true,	   ['combinable'] = nil,   ['description'] = 'Box of Shrimp'},
 	['crayfish'] 		 			 = {['name'] = 'crayfish', 						['label'] = 'Cray Fish', 				['weight'] = 1000, 		['type'] = 'item', 		['image'] = 'crayfish.png', 			['unique'] = true, 		['useable'] = false, 	['shouldClose'] = true,	   ['combinable'] = nil,   ['description'] = 'Cray Fish'},
