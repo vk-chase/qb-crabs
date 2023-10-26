5acd39e77cc89524ee059c86a102ffc3012e712b
Chase-Scripts <112039422+vk-chase@users.noreply.github.com>
2023-10-25 23:08:26 -0500

    Update config.lua
    x

--- config.lua
+++ config.lua
@@ -1,21 +1,45 @@
-Config = {}
-
-Config.PickupBlip = {x = -2334.1  , y = 2764.7 ,z = 3.15}
-Config.CrabProcessingSpot = {x = -3281.87 , y = 976.09, z = 8.35}
-Config.ShrimpProcessSpot = {x = -3282.09 , y = 958.72, z = 8.35}
-Config.SellingSpot = {x = -1042.29 , y = -1353.84 , z = 5.53}
-
-Config.ShrimpPerBox = 4 -- shrimp required to process
-Config.ShrimBoxRequired = 1 -- boxes required to sell the processed shrimp
-Config.CrabPerBox = 5   -- crabs required to process
-Config.CrawfishRequired = 2--amount of processed crabs aka crawfish you need to sell
+Config = Config or {}
 
 Config.PayType = 'bank' -- 'cash' or 'bank'
-Config.ProcessedShrimpPayout = 500 -- ^ amount you get for selling the required boxes ^
-Config.CrawFishPayout = 500
-
-
-Config.BlipShrimpProcess = "Shrimp Processing"
-Config.CrabHarvestingBlip = "Crab Harvesting"
-Config.CrabProcessingBlip = "Crab Processing"
+Config.NotifyType 'rtx' -- 'rtx' 'qb' or 'okok'
+--### BLIPS ###--
+Config.PickupBlip = {x = -2334.1  , y = 2764.7 ,z = 3.15}
+Config.DigsiteVector = vector3(-2334.10, 2764.70, 3.15)
+Config.SellingSpot = {x = -1039.09, y = -1353.44, z = 5.53}
+Config.DigsiteName = "Digging Site"
 Config.SellBlip = "SeaFood Sales"
+
+
+
+
+
+Config.SellSeafoodVector = vector3(-1039.09, -1353.44, 5.55)
+Config.SellReturn = vector3(-1039.09, -1353.44, 5.55) --### blip location ### to sell fish, can be same as next vecotr3
+Config.SellReturnLocation = {
+   [1] = {
+           coords = vector3(-1039.09, -1353.44, 5.55), --sell coordz for target
+           length = 1,
+           width = 1,
+           heading = 0,
+           debugPoly = false,
+           minZ = 5.53-2,
+           maxZ = 5.53+2,
+           distance = 3.0
+       },
+   }
+Config.BankMoney = false -- false= cash || true= bank
+Config.UseTimes = false -- false= sell fish anytime || true sell between hours 
+Config.TimeOpen = 7 -- Opening Time
+Config.TimeClosed = 17 -- Closing Time
+
+--[[ --####### BY DEFAULT #######--
+    crawfish = 60% chance on success
+    shrimp = 20% chance on success
+    crabs = 20% chance on success
+]]
+Config.ReturnItems = {   
+    { item = 'crayfish',  price = math.random(5,20) },--these are randomized and will randomize every restart
+    { item = 'shrimp',  price = math.random(100,200) },
+    { item = 'crabs',  price = math.random(120,220) },
+ 
+ }
