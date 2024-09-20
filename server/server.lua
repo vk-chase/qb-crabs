e4dfb0316850ef54b8aed178aa47af63a897e3f7
Chase-Scripts <112039422+vk-chase@users.noreply.github.com>
2024-09-20 03:00:10 -0500

    Update server.lua
    clean up.
    made loot table.

--- server/server.lua
+++ server/server.lua
@@ -1,19 +1,28 @@
 local QBCore = exports['qb-core']:GetCoreObject()
---################## DIGGING ##################
+
+local RewardItems = {
+    {item = "crayfish", chance = 40, min = 1, max = 8},
+    {item = "shrimp", chance = 20, min = 1, max = 2},
+    {item = "crabs", chance = 20, min = 1, max = 2},
+						-- add more
+}
+
+local function AddItemAndNotify(Player, item, amount)
+    Player.Functions.AddItem(item, amount)
+    TriggerClientEvent('inventory:client:ItemBox', Player.PlayerData.source, QBCore.Shared.Items[item], "add")
+end
+
 RegisterServerEvent("mudbug:server:getreward", function()
-	local src = source
-	local Player = QBCore.Functions.GetPlayer(src)
-	local chancex = math.random(1,100)
-    if chancex < 40 then
-			Player.Functions.AddItem("crayfish", math.random(1,8))
-			TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items["shrimp"], "add")
-	end
-    if chancex >= 21 or chancex <= 40 then
-		Player.Functions.AddItem("shrimp", math.random(1,2))
-		TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items["shrimp"], "add")
-	end
-	if chancex <= 20 then
-		Player.Functions.AddItem("crabs", math.random(1,2))
-		TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items["crabs"], "add")
-	end
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    local chance = math.random(1, 100)
+    
+    for _, reward in ipairs(RewardItems) do
+        if chance <= reward.chance then
+            local amount = math.random(reward.min, reward.max)
+            AddItemAndNotify(Player, reward.item, amount)
+            break
+        end
+        chance = chance - reward.chance
+    end
 end)
